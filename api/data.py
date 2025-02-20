81737a97df837a3fd293038de58402a742cb99fd
engelbert teh <engelbertteh.dev@gmail.com>
2025-02-20 15:04:11 +0800

    added df

---
+++ api/data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_csv('banking_data.csv')
+
+def get_data():
+  return df 
--- api/index.py
+++ api/index.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+import data
 
 app = Flask(__name__)
 
+data_list = data.get_data().to_dict(orient="records") # list
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return jsonify(data_list)
 
 @app.route('/about')
 def about():
     return 'About'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
