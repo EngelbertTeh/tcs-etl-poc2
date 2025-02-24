5b6faf0de2a4218b728021bf893a8f863284a84a
engelbert teh <engelbertteh.dev@gmail.com>
2025-02-24 20:11:55 +0800

    removed mysql

--- services/etl/load.py
+++ services/etl/load.py
@@ -1,17 +1,17 @@
-import mysql.connector as mysql
+# import mysql.connector as mysql
 
-conn = mysql.connect(
-  host='localhost',
-  user='root',
-  password='Barney1234!',
-  database='tcs_etl'
-)
+# conn = mysql.connect(
+#   host='localhost',
+#   user='root',
+#   password='Barney1234!',
+#   database='tcs_etl'
+# )
 
-def load_data(df):
-  cursor = conn.cursor()
+# def load_data(df):
+#   cursor = conn.cursor()
 
-  cursor.execute('insert into data_mart (date, open, high, low, close, volume, dividends, stock_splits) values (?, ?, ?, ?, ?, ?, ?, ?)', df)
-  data = cursor.fetchall()
-  print('data is ')
-  print(data)
+#   cursor.execute('insert into data_mart (date, open, high, low, close, volume, dividends, stock_splits) values (?, ?, ?, ?, ?, ?, ?, ?)', df)
+#   data = cursor.fetchall()
+#   print('data is ')
+#   print(data)
 
